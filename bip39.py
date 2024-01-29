586267057d0c418cfe0e05ecea015e2745a19e02
TheRealHedgeLord <therealhedgelord@gmail.com>
2024-01-29 19:00:19 +0800

    Update bip39.py

--- bip39.py
+++ bip39.py
@@ -2,6 +2,7 @@ import json
 import random
 import hashlib
 
+from functools import cache, cached_property
 from typing import List, Dict
 from enum import StrEnum
 
@@ -18,7 +19,7 @@ with open("./english.json", mode="r") as f:
 
 
 def checksum(seed: List[int]) -> int:
-    if len(seed) not in (11, 23):
+    if len(seed) != 23:
         raise Exception("invalid seed length")
     return random.randint(0, 2047)
 
@@ -26,30 +27,33 @@ def checksum(seed: List[int]) -> int:
 class Seed:
     def __init__(self, seed: List[int] | None = None) -> None:
         if not seed:
-            random_seed = [random.randint(0, 2048) for _ in range(23)]
+            random_seed = [random.randint(0, 2047) for _ in range(23)]
             self.seed = random_seed + [checksum(random_seed)]
         elif checksum(seed[0:-1]) != seed[-1]:
             raise Exception("invalid checksum")
         else:
             self.seed = seed
 
-    @property
-    def hex(self):
+    @cached_property
+    def bytes(self):
         binary = "".join(["{0:011b}".format(i) for i in self.seed])
-        return f"{int(binary, 2):0{(len(binary)+3)//4}x}"
+        return int(binary, 2).to_bytes(length=33, byteorder="big", signed=False)
 
+    @cache
     def digest(self, language: Language = Language.english) -> str:
         return " ".join([_DIGEST[language][i] for i in self.seed])
 
     def create_shadow_seed(
         self, passphrase: str, language: Language | None = None
     ) -> List[int] | str:
-        suffix = hashlib.sha256(passphrase.encode("utf-8")).hexdigest()
-        hex_value = hashlib.sha512((self.hex + suffix).encode("utf-8")).hexdigest()
-        bin_value = "{0:0512b}".format(int(hex_value, 16))
+        suffix = hashlib.sha256(passphrase.encode("utf-8")).digest()
+        shadow_seed_bytes = hashlib.sha256(self.bytes + suffix).digest()
+        shadow_seed_binary = "{0:0256b}".format(
+            int.from_bytes(shadow_seed_bytes, byteorder="big", signed=False)
+        )
         shadow_seed = []
         for i in range(23):
-            int_value = int(bin_value[i * 11 : i * 11 + 11], 2)
+            int_value = int(shadow_seed_binary[i * 11 : i * 11 + 11], 2)
             shadow_seed.append(int_value)
         shadow_seed.append(checksum(shadow_seed))
         return (
