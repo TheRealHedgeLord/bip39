e006a5609d2a7b7f77a944355df16f052b48b962
TheRealHedgeLord <therealhedgelord@gmail.com>
2024-01-29 20:14:53 +0800

    Update bip39.py

--- bip39.py
+++ bip39.py
@@ -18,20 +18,26 @@ with open("./english.json", mode="r") as f:
     _DIGEST[Language.english] = json.load(f)
 
 
-def checksum(seed: List[int]) -> int:
-    if len(seed) != 23:
-        raise Exception("invalid seed length")
-    return random.randint(0, 2047)
+@cache
+def checksum(bits: str) -> List[int]:
+    if len(bits) != 256:
+        raise Exception("invalid bit length")
+    checksum_hex = hashlib.sha256(
+        int(bits, 2).to_bytes(length=32, byteorder="big", signed=False)
+    ).hexdigest()[0:2]
+    complete_seed_bits = bits + "{0:08b}".format(int(checksum_hex, 16))
+    return [int(complete_seed_bits[i * 11 : i * 11 + 11], 2) for i in range(24)]
 
 
 class Seed:
     def __init__(self, seed: List[int] | None = None) -> None:
         if not seed:
-            random_seed = [random.randint(0, 2047) for _ in range(23)]
-            self.seed = random_seed + [checksum(random_seed)]
-        elif checksum(seed[0:-1]) != seed[-1]:
-            raise Exception("invalid checksum")
+            random_256_bits = "{0:0256b}".format(random.randint(0, 2**256 - 1))
+            self.seed = checksum(random_256_bits)
         else:
+            bits = "".join(["{0:011b}".format(i) for i in seed])
+            if checksum(bits[0:256]) != seed:
+                raise Exception("invalid checksum")
             self.seed = seed
 
     @cached_property
@@ -48,14 +54,10 @@ class Seed:
     ) -> List[int] | str:
         suffix = hashlib.sha256(passphrase.encode("utf-8")).digest()
         shadow_seed_bytes = hashlib.sha256(self.bytes + suffix).digest()
-        shadow_seed_binary = "{0:0256b}".format(
+        shadow_seed_bits = "{0:0256b}".format(
             int.from_bytes(shadow_seed_bytes, byteorder="big", signed=False)
         )
-        shadow_seed = []
-        for i in range(23):
-            int_value = int(shadow_seed_binary[i * 11 : i * 11 + 11], 2)
-            shadow_seed.append(int_value)
-        shadow_seed.append(checksum(shadow_seed))
+        shadow_seed = checksum(shadow_seed_bits)
         return (
             shadow_seed
             if not language
